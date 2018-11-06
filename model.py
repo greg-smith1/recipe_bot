d66bf9af847bbd60526f318db7d880c48e3f5798
Greg Smith <greg.smith3492@gmail.com>
2018-11-06 16:36:17 -0500

    update enter/exit methods

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ class Database:
     def __init__(self, db):
         self.db = db
 
-    def __enter__(self, db):
+    def __enter__(self):
         self.connection = sqlite3.connect(db)
         self.cursor = self.connection.cursor()
         return self
 
-    def __exit__(self, ):
+    def __exit__(self, exc_type, exc_value, trace):
         pass
