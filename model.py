b61fef66e2e77327966b9f6f851cb20e83ab4881
Greg Smith <greg.smith3492@gmail.com>
2018-11-06 21:11:18 -0500

    add full recipe class, finished __exit__

--- model.py
+++ model.py
@@ -15,3 +15,30 @@ class Database:
             self.connection.commit()
         self.cursor.close()
         self.connection.close()
+
+class Recipe:
+
+    def __init__(self, title, cook_time, ingredients, steps=None, link=None):
+        """Arguments:
+
+        title - str, name of the recipe
+        cook_time - int, cooking time in minutes, including preparation
+        ingredients - dict, containing item:measurement pairs
+        steps - list of strings, containing each step in the recipe
+            default None, for easy one-pot/one-step recipes
+        link - source link of recipe if obtained online
+        """
+        self.title = title
+        self.cook_time = cook_time
+        self.ingredients = ingredients
+        self.steps = steps
+        self.link = link
+
+    def print_steps(self):
+        for i in range(len(self.steps)):
+            print(i+1, self.steps[i])
+
+    def __str__(self):
+        print(self.title)
+        for item, measurement in self.ingredients:
+            print(item, measurement)
