b432e5f83a528fac7b391f9340b2699fee10699e
Greg Smith <greg.smith3492@gmail.com>
2018-11-14 15:52:30 -0500

    add separate tests file

--- model.py
+++ model.py
@@ -52,10 +52,3 @@ class Recipe:
             print(item, measurement)
 
 
-if __name__=='__main__':
-    steps = ['First step', 'Second step', 'Third step', 'And you\'re done!']
-    ingredients = [('Carrots', '2 Cups'), ('Chocolate', '1tsp'), ('Mayonnaise', '1 Jar')]
-    example = Recipe('Test Recipe', 35, ingredients, steps=steps)
-    example.print_steps()
-    example.__str__()
-    
